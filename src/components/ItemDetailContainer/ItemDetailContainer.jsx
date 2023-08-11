67a07141c7b5086709fca1a88b1dd920395d8040
Alan Maciel <alan.ivan.maciel@gmail.com>
2023-08-11 11:43:25 -0300

    Implementacion de Loader

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,19 +2,27 @@ import { useEffect, useState } from "react";
 import { getDataByID } from "../../services/asyncMock";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+import "../ItemListContainer/ItemListContainer.css";
 
 function ItemDetailContainer() {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState(null);
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function requestProduct() {
       const respuesta = await getDataByID(id);
       setProduct(respuesta);
+      setIsLoading(false);
     }
 
     requestProduct();
   }, []);
+
+  if (isLoading) {
+    return <CircularProgress className="loader" />;
+  }
 
   return (
     <div>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,23 +3,35 @@ import { getData, getCategoryId } from "../../services/asyncMock";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
 import "./ItemListContainer.css";
+import { CircularProgress } from "@mui/material";
 
 const ItemListContainer = ({ greeting }) => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { categoryId } = useParams();
 
   let title = categoryId ? categoryId.toUpperCase() : "Todos los productos";
 
   useEffect(() => {
+    setIsLoading(true);
     async function requestProducts() {
       let respuesta = categoryId
         ? await getCategoryId(categoryId)
         : await getData();
       setProducts(respuesta);
+      setIsLoading(false);
     }
 
     requestProducts();
   }, [categoryId]);
+
+  if (isLoading) {
+    return <CircularProgress className="loader" />;
+  }
+
+  if (products.length === 0) {
+    return <p>No hay productos disponibles para esa consulta.</p>;
+  }
 
   return (
     <div className="ItemContainer">
